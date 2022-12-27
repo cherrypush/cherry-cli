4bc17ed8984da8a3c9b8094670a54271e00249de
Romain Choquet <romain.choquet@gmail.com>
2022-12-27 11:39:42 +0100

    fixes & contributions improvements

--- src/contributions.js
+++ src/contributions.js
@@ -4,6 +4,7 @@ import * as git from './git.js'
 import { findOccurrences } from './occurences.js'
 import { buildFilesAtSha } from './files.js'
 import uniq from 'lodash/uniq.js'
+import pLimit from 'p-limit'
 
 const getOccurencesCount = async (configuration, paths, sha) => {
   const files = await buildFilesAtSha(paths, sha)
@@ -11,37 +12,40 @@ const getOccurencesCount = async (configuration, paths, sha) => {
   return mapValues(groupBy(occurrences, 'metric_name'), (occurrences) => occurrences.length)
 }
 
-const getCurrentAndPrevious = async (configuration, currentSha, previousSha) => {
-  const paths = await git.changedFiles(currentSha)
-  return await Promise.all([
-    getOccurencesCount(configuration, paths, currentSha),
-    getOccurencesCount(configuration, paths, previousSha),
+const getCommitContribution = async (configuration, currentCommit, previousCommit) => {
+  const paths = await git.changedFiles(currentCommit.sha)
+  const [current, previous] = await Promise.all([
+    getOccurencesCount(configuration, paths, currentCommit.sha),
+    getOccurencesCount(configuration, paths, previousCommit.sha),
   ])
+  const metrics = uniq(Object.keys(current).concat(Object.keys(previous)))
+  const deltaByMetric = {}
+
+  metrics.forEach((metric) => {
+    const delta = (current[metric] || 0) - (previous[metric] || 0)
+    if (delta !== 0) deltaByMetric[metric] = delta
+  })
+
+  return {
+    date: currentCommit.isoDate,
+    authorName: currentCommit.authorName,
+    authorEmail: currentCommit.authorEmail,
+    sha: currentCommit.sha,
+    metrics: deltaByMetric,
+  }
 }
 
-export const findContributors = async (configuration, beginSha, endSha) => {
+export const findContributions = async (configuration, beginSha, endSha) => {
   const commits = await git.getCommits(beginSha, endSha)
-  let contributions = {}
-
   const promises = []
+  // Avoid "Error: spawn EBADF" when invoking too many shell commands, limiting to 10 does not impact run time
+  const limit = pLimit(10)
+
   for (let i = 0; i < commits.length - 1; i++) {
     const currentCommit = commits[i]
     const previousCommit = commits[i + 1]
-    promises.push(
-      getCurrentAndPrevious(configuration, currentCommit.sha, previousCommit.sha).then(([current, previous]) => {
-        console.log(`Retrieving contributors for ${currentCommit.sha}`)
-        const metrics = uniq(Object.keys(current).concat(Object.keys(previous)))
-        metrics.forEach((metric) => {
-          const author = `${currentCommit.authorName} <${currentCommit.authorEmail}>`
-          const delta = (current[metric] || 0) - (previous[metric] || 0)
-          if (delta !== 0) {
-            contributions[metric] ||= {}
-            contributions[metric][author] = (contributions[metric][author] || 0) + delta
-          }
-        })
-      })
-    )
+    promises.push(limit(() => getCommitContribution(configuration, currentCommit, previousCommit)))
   }
-  await Promise.all(promises)
-  console.log(contributions)
+
+  return await Promise.all(promises)
 }
