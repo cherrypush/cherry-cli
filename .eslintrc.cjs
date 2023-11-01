0278b679aaf207fe27e65c575a4a25f5c14fe688
Flavio Wuensche <f.wuensche@gmail.com>
2023-11-01 14:20:29 +0100

    cleanup project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,14 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     es2021: true,
+    jest: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: 'eslint:recommended',
   overrides: [],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-  },
+  rules: {},
 }
--- test/factories/authorization_request.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require "factory_bot"
-
-FactoryBot.define do
-  factory :authorization_request do
-  end
-end
