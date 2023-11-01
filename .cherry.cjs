59d269724374da218e49e711bbea213aa9afb37e
Flavio Wuensche <f.wuensche@gmail.com>
2023-11-01 15:02:44 +0100

    support cjs files

---
+++ .cherry.cjs
@@ -0,0 +1,19 @@
+const JS_FILES = 'app/**/*.{js,jsx}'
+const TS_FILES = 'app/**/*.{ts,tsx}'
+
+module.exports = {
+  project_name: 'fwuensche/cherry-cli',
+  plugins: {
+    npmOutdated: {},
+    loc: {},
+    eslint: {},
+    jsCircularDependencies: { include: 'src/**' },
+    jsUnimported: {},
+  },
+  metrics: [
+    {
+      name: 'TODO',
+      pattern: /TODO/,
+    },
+  ],
+}
