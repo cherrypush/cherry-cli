02fa214e3ba3fbaa9ee12f291de26c4bb396f5eb
Flavio Wuensche <f.wuensche@gmail.com>
2023-11-25 10:53:59 +0100

    handle multiple metrics on cherry diff and close #12

--- test/run.test.js
+++ test/run.test.js
@@ -28,13 +28,3 @@ describe('cherry run', () => {
     })
   }, 10000)
 })
-
-describe('cherry diff', () => {
-  test('should exit with an error if --api-key is missing', (done) => {
-    exec('node bin/cherry.js diff --metric test', (error, _stdout, stderr) => {
-      expect(error.code).toBe(1)
-      expect(stderr).toContain('API key is missing')
-      done()
-    })
-  })
-})
