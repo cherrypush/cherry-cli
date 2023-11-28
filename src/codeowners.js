b6b88332912799d94d17d6221dac3dcbf5799dbe
Julien Tanay <julien.tanay@gmail.com>
2023-11-28 15:36:17 +0100

    chore: add husky,lint-staged,prettier (#23)
    * chore: add husky,lint-staged,prettier
    
    * chore: add ci checks
    
    * chore: add missing husky hook
    
    ---------
    
    Co-authored-by: Flavio Wuensche <f.wuensche@gmail.com>

--- src/codeowners.js
+++ src/codeowners.js
@@ -1,64 +1,77 @@
-import { findUpSync } from 'find-up'
-import fs from 'fs'
-import glob from 'glob'
-import intersection from 'lodash/intersection.js'
-import uniq from 'lodash/uniq.js'
-import path from 'path'
-import trueCasePath from 'true-case-path'
-import { isDirectory } from './file.js'
+import { findUpSync } from "find-up";
+import fs from "fs";
+import glob from "glob";
+import intersection from "lodash/intersection.js";
+import uniq from "lodash/uniq.js";
+import path from "path";
+import trueCasePath from "true-case-path";
+import { isDirectory } from "./file.js";
 
-const { trueCasePathSync } = trueCasePath
+const { trueCasePathSync } = trueCasePath;
 
 class Codeowners {
   constructor() {
-    this.ownersByFile = {}
-    this.init()
+    this.ownersByFile = {};
+    this.init();
   }
 
   init() {
-    const fileName = 'CODEOWNERS'
+    const fileName = "CODEOWNERS";
 
     const codeownersPath = findUpSync(
-      [`.github/${fileName}`, `.gitlab/${fileName}`, `docs/${fileName}`, `${fileName}`],
-      { cwd: process.cwd() }
-    )
+      [
+        `.github/${fileName}`,
+        `.gitlab/${fileName}`,
+        `docs/${fileName}`,
+        `${fileName}`,
+      ],
+      { cwd: process.cwd() },
+    );
 
-    if (!codeownersPath) return
+    if (!codeownersPath) return;
 
-    const codeownersFilePath = trueCasePathSync(codeownersPath)
-    let codeownersDirectory = path.dirname(codeownersFilePath)
+    const codeownersFilePath = trueCasePathSync(codeownersPath);
+    let codeownersDirectory = path.dirname(codeownersFilePath);
 
     // We might have found a bare codeowners file or one inside the three supported subdirectories.
     // In the latter case the project root is up another level.
-    if (codeownersDirectory.match(/\/(.github|.gitlab|docs)$/i)) codeownersDirectory = path.dirname(codeownersDirectory)
+    if (codeownersDirectory.match(/\/(.github|.gitlab|docs)$/i))
+      codeownersDirectory = path.dirname(codeownersDirectory);
 
-    const codeownersFile = path.basename(codeownersFilePath)
+    const codeownersFile = path.basename(codeownersFilePath);
 
     if (codeownersFile !== fileName)
-      throw new Error(`Found a ${fileName} file but it was lower-cased: ${codeownersFilePath}`)
+      throw new Error(
+        `Found a ${fileName} file but it was lower-cased: ${codeownersFilePath}`,
+      );
 
     if (isDirectory(codeownersFilePath))
-      throw new Error(`Found a ${fileName} but it's a directory: ${codeownersFilePath}`)
+      throw new Error(
+        `Found a ${fileName} but it's a directory: ${codeownersFilePath}`,
+      );
 
     const lines = fs
       .readFileSync(codeownersFilePath)
       .toString()
       .split(/\r\n|\r|\n/)
       .filter(Boolean)
-      .map((line) => line.trim())
+      .map((line) => line.trim());
 
     for (const line of lines) {
-      if (line.startsWith('#')) continue
+      if (line.startsWith("#")) continue;
 
-      const [codeownersPath, ...owners] = line.split(/\s+/)
-      for (const file of this.#globFiles(codeownersPath)) this.ownersByFile[file] = uniq(owners)
+      const [codeownersPath, ...owners] = line.split(/\s+/);
+      for (const file of this.#globFiles(codeownersPath))
+        this.ownersByFile[file] = uniq(owners);
     }
   }
 
   #globFiles(codeownersPath) {
-    if (codeownersPath.includes('*')) return glob.sync(codeownersPath, { nodir: true })
-    if (isDirectory(codeownersPath)) return glob.sync(path.join(codeownersPath, '**/*'), { nodir: true })
-    return [codeownersPath]
+    if (codeownersPath.includes("*"))
+      return glob.sync(codeownersPath, { nodir: true });
+    if (isDirectory(codeownersPath))
+      return glob.sync(path.join(codeownersPath, "**/*"), { nodir: true });
+    return [codeownersPath];
   }
 
   getFiles(owners) {
@@ -66,13 +79,13 @@ class Codeowners {
       Object.entries(this.ownersByFile)
         .filter(([, fileOwners]) => intersection(owners, fileOwners).length > 0)
         .map(([file]) => file)
-        .flat()
-    )
+        .flat(),
+    );
   }
 
   getOwners(file) {
-    return this.ownersByFile[file] || []
+    return this.ownersByFile[file] || [];
   }
 }
 
-export default Codeowners
+export default Codeowners;
--- src/contributions.js
+++ src/contributions.js
@@ -1,20 +1,22 @@
-import _ from 'lodash'
+import _ from "lodash";
 
 const toCountByMetricName = (occurrences) =>
-  _.mapValues(_.groupBy(occurrences, 'metricName'), (occurrences) =>
-    _.sum(occurrences.map((occurrence) => occurrence.value || 1))
-  )
+  _.mapValues(_.groupBy(occurrences, "metricName"), (occurrences) =>
+    _.sum(occurrences.map((occurrence) => occurrence.value || 1)),
+  );
 
 export const computeContributions = (occurrences, previousOccurrences) => {
-  const counts = toCountByMetricName(occurrences)
-  const previousCounts = toCountByMetricName(previousOccurrences)
+  const counts = toCountByMetricName(occurrences);
+  const previousCounts = toCountByMetricName(previousOccurrences);
 
-  const metrics = _.uniq(Object.keys(counts).concat(Object.keys(previousCounts)))
-  const contributions = []
+  const metrics = _.uniq(
+    Object.keys(counts).concat(Object.keys(previousCounts)),
+  );
+  const contributions = [];
   metrics.forEach((metric) => {
-    const diff = (counts[metric] || 0) - (previousCounts[metric] || 0)
-    if (diff !== 0) contributions.push({ metricName: metric, diff })
-  })
+    const diff = (counts[metric] || 0) - (previousCounts[metric] || 0);
+    if (diff !== 0) contributions.push({ metricName: metric, diff });
+  });
 
-  return contributions
-}
+  return contributions;
+};
