49863d85454643ec8d236d904251e56db917a10e
Flavio Wuensche <f.wuensche@gmail.com>
2023-12-10 19:08:29 +0100

    wip

--- bin/commands/diff.js
+++ bin/commands/diff.js
@@ -53,7 +53,7 @@ export default function (program) {
       for (const metric of metrics) {
         try {
           console.log('-----------------------------------')
-          console.log(metric)
+          console.log(`Metric: ${metric}`)
 
           if (inputFile) {
             const content = fs.readFileSync(inputFile, 'utf8')
