6a1cc35a3f5a167fcec9b3a550c739e945c77d08
Flavio Wuensche <f.wuensche@gmail.com>
2023-12-11 19:31:47 +0100

    wip

--- src/git.js
+++ src/git.js
@@ -32,8 +32,10 @@ export const guessProjectName = async () => {
 
 export const sha = async () => (await git('rev-parse HEAD')).toString()
 
+export const currentBranchName = async () => (await git('rev-parse --abbrev-ref HEAD')).toString().trim()
+
 // TODO: this should automatically get the current branch and the base branch, then use then to get the base sha
-export const mergeBaseSha = async () => (await git('merge-base main cherry-diff-in-isolation')).toString()
+export const mergeBaseSha = async () => (await git(`merge-base main ${currentBranchName}`)).toString()
 
 export const authorName = async (sha) => (await git(`show ${sha} --format=%an --no-patch`))[0]
 
