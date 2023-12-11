5a28c13f13d9ff5c6f01b07e3e7548fd2f60f181
Flavio Wuensche <f.wuensche@gmail.com>
2023-12-11 20:05:21 +0100

    wip

--- test/cherry_diff.test.js
+++ test/cherry_diff.test.js
@@ -10,16 +10,16 @@ beforeEach(() => {
 })
 
 describe('cherry diff', () => {
-  test('should exit with an error if --api-key is missing', (done) => {
+  test('should ignore the fact that an --api-key is missing', (done) => {
     exec('node bin/cherry.js diff --metric test', (error, _stdout, stderr) => {
-      expect(error.code).toBe(1)
+      expect(error).toBe(null)
       expect(stderr).toContain('API key is missing') // TODO: cherry diff should not depend on API key
       done()
     })
   })
 
   test('should exit with an error if --metric is missing', (done) => {
-    exec('node bin/cherry.js diff --api-key test', (error, _stdout, stderr) => {
+    exec('node bin/cherry.js diff', (error, _stdout, stderr) => {
       expect(error.code).toBe(1)
       expect(stderr).toContain(`required option '--metric <metric>' not specified`)
       done()
