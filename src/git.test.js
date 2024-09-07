6ff6da1ad809b241945aeb50c228a39da1076986
Flavio Wuensche <flavio.wuensche@doctolib.com>
2024-09-07 15:11:28 +0200

    chore: add fixture project to allow for better testing

--- src/git.test.js
+++ src/git.test.js
@@ -1,6 +1,7 @@
-import { describe, expect, it, vi } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest'
 
-import { guessProjectName } from './git.js' // Import the function to test
+import { guessProjectName } from './git.js'
+import path from 'path'
 
 // Partially mock the module
 vi.mock('./git.js', async (importOriginal) => {
@@ -11,13 +12,20 @@ vi.mock('./git.js', async (importOriginal) => {
   }
 })
 
-describe('guessProjectName', () => {
-  it('should return an empty string if no remotes are found', async () => {
-    const { git } = await import('./git.js') // Import the mocked `git` function
+// We have a fixture project in `test/fixtures/project_one` that we use to create test scenarios
+const originalCwd = process.cwd()
+const fixturesPath = path.join(originalCwd, 'test/fixtures/project_one')
 
+describe('guessProjectName', () => {
+  beforeAll(() => process.chdir(fixturesPath)) // Change to `test/fixtures/project_one`
+  afterAll(() => process.chdir(originalCwd)) // Change back to the original working directory
+
+  it('should return an empty string if no remotes are found', async () => {
+    // TODO: It'd be better to improve guessProjectName to take url as a param, so we can test it without mocking git
+    const { git } = await import('./git.js') // Import the mocked `git` function
     git.mockResolvedValueOnce([]) // Mock `git('remote')` to return an empty array
     const result = await guessProjectName()
 
-    expect(result).toBe('cherrypush/cherry-cli')
+    expect(result).toBe('cherrypush/project_one')
   })
 })
