5c9378ab5b028d222b2d0cb5b55a9e7996700c22
Flavio Wuensche <flavio.wuensche@doctolib.com>
2024-09-10 01:00:07 +0200

    migrate to ts

--- src/sh.ts
+++ src/sh.ts
@@ -2,7 +2,7 @@ import child_process from 'child_process'
 import { debug } from './log.js'
 
 // From https://stackoverflow.com/a/68958420/9847645, to avoid 200Kb limit causing ENOBUFS errors for large output
-const sh = (cmd, { throwOnError = true } = {}) =>
+const sh = (cmd: string, { throwOnError = true } = {}) =>
   new Promise((resolve, reject) => {
     debug('#', cmd)
     const [command, ...args] = cmd.split(/\s+/)
@@ -14,7 +14,7 @@ const sh = (cmd, { throwOnError = true } = {}) =>
     spawnedProcess.stdout.on('data', (chunk) => (stdout += chunk.toString()))
     spawnedProcess.stderr.on('data', (chunk) => (stderr += chunk.toString()))
     spawnedProcess.on('close', (code) => {
-      if (throwOnError && code > 0) return reject(new Error(`${stderr} (Failed Instruction: ${cmd})`))
+      if (throwOnError && code && code > 0) return reject(new Error(`${stderr} (Failed Instruction: ${cmd})`))
       debug(stdout)
       resolve({ stderr, stdout })
     })
