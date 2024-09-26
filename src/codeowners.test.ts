2660d97697dea8e352cb05dd12e7d2006e0780c1
Flavio Wuensche <flavio.wuensche@doctolib.com>
2024-09-26 11:46:58 +0200

    fix codeowners

--- src/codeowners.test.ts
+++ src/codeowners.test.ts
@@ -4,20 +4,26 @@ import Codeowners from './codeowners.js'
 
 describe('getOwners', () => {
   // Assuming src folder is owned by @fwuensche and @rchoquet
-  it('returns returns @fwuensche for files inside src', async () => {
+  it('recognizes folder patterns', async () => {
     const codeowners = new Codeowners()
     expect(codeowners.getOwners('src/codeowners.test.ts')).toEqual(['@fwuensche', '@rchoquet'])
   })
 
   // Assuming bin folder has no defined owners, but @fwuensche is the default owner
-  it('returns an empty array for files inside bin', async () => {
+  it('recognizes default owners', async () => {
     const codeowners = new Codeowners()
     expect(codeowners.getOwners('bin/commands/run.ts')).toEqual(['@fwuensche'])
   })
 
   // Assuming js files are owned by @rchoquet
-  it('returns @fwuensche for js files', async () => {
+  it('recognizes file extension patterns', async () => {
     const codeowners = new Codeowners()
     expect(codeowners.getOwners('bin/codeowners.js')).toEqual(['@rchoquet'])
+  })
+
+  // Assuming the file does not exist, but matches an existing pattern
+  it('returns who would theoretically own the file even tho it does not exist', async () => {
+    const codeowners = new Codeowners()
+    expect(codeowners.getOwners('bin/non-existing-file')).toEqual(['@fwuensche'])
   })
 })
