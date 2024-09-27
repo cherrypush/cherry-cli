343cf10377f705086fbfd239973415868718fdbe
Flavio Wuensche <f.wuensche@gmail.com>
2024-09-27 14:22:45 +0200

    fix (#109)

--- src/codeowners.test.ts
+++ src/codeowners.test.ts
@@ -5,26 +5,31 @@ import Codeowners from './codeowners.js'
 describe('getOwners', () => {
   test('/src/ pattern matches files inside src folder', async () => {
     const codeowners = new Codeowners()
-    expect(codeowners.getOwners('src/codeowners.test.ts')).toEqual(['@fwuensche', '@rchoquet'])
+    expect(codeowners.getOwners('src/codeowners.test.ts')).toEqual(['@source'])
   })
 
   test('plugins/ pattern should match any folder named plugins', async () => {
     const codeowners = new Codeowners()
-    expect(codeowners.getOwners('src/plugins/eslint.js')).toEqual(['@fwuensche'])
+    expect(codeowners.getOwners('src/plugins/loc.js')).toEqual(['@plugins'])
   })
 
   test('defaults to the root owner', async () => {
     const codeowners = new Codeowners()
-    expect(codeowners.getOwners('bin/commands/push.ts')).toEqual(['@fwuensche'])
+    expect(codeowners.getOwners('bin/commands/push.ts')).toEqual(['@root'])
   })
 
   test('*.js also matches files from subfolders', async () => {
     const codeowners = new Codeowners()
-    expect(codeowners.getOwners('bin/commands/diff.js')).toEqual(['@rchoquet'])
+    expect(codeowners.getOwners('bin/commands/diff.js')).toEqual(['@javascript'])
   })
 
   test('non existing files return an empty list of owners', async () => {
     const codeowners = new Codeowners()
     expect(codeowners.getOwners('bin/non-existing-file')).toEqual([])
+  })
+
+  test('/src/plugins/eslint.ts pattern should override previous ones', async () => {
+    const codeowners = new Codeowners()
+    expect(codeowners.getOwners('src/plugins/eslint.js')).toEqual(['@eslint-plugin'])
   })
 })
