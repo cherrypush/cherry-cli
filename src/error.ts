db864192b86b0b7e4af5a8dad372ff191a5194b9
Flavio Wuensche <f.wuensche@gmail.com>
2024-10-04 19:35:27 +0200

    fix

--- src/error.ts
+++ src/error.ts
@@ -1,10 +1,10 @@
 export const panic = (error: unknown): never => {
   if (typeof error === 'string') {
-    console.log(error)
+    console.error(error)
   } else if (error instanceof Error) {
-    console.log(error.message)
+    console.error(error.message)
   } else {
-    console.log('An unknown error occurred')
+    console.error('An unknown error occurred')
   }
 
   process.exit(1)
